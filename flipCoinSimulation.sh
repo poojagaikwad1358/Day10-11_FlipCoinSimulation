297466b190b81daee012ace51fdde2d4a4eba759
poojagaikwad1358 <poojagaikwad1358@gmail.com>
2020-07-08 00:16:52 +0530

    modify UseCase1 till either head or tail won 21 times

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -33,3 +33,34 @@ done
 
 echo "Total Number Of Head Win is:  $isHeadWon"
 echo "Total Number Of Tail Win is: $isTailWon"
+
+#UseCase3- Modify the earlier UC 2 to continue till either of
+	   #them have won 21 times. Show if it’s a Win or Tie.
+	   #If Win then who won and by how much
+
+while [[ $isHeadWon -lt 21 && $isTailWon -lt 21 ]]
+do
+	randomCheck=$(( RANDOM%2 ))
+
+if [[ isHead -eq randomCheck ]]
+then
+	echo "Tail"
+	isHeadWon=$(( $isHeadWon + 1 ))
+else
+	echo "Head"
+	isTailWon=$(( $isTailWon + 1 ))
+fi
+done
+
+echo "Total Number Of Head Win is: $isHeadWon"
+echo "Total Number Of Tail Win is: $isTailWon"
+
+if [[ isHeadWon -lt isTailWon ]]
+then
+	echo "Tail Win"
+elif [[ isHeadWon -eq isTailWon ]]
+then
+	echo "Tie"
+else
+	echo "Head Win"
+fi
